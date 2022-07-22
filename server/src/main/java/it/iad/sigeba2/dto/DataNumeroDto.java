8228b483b8efca7f7e3547a71fba4a2aeab1114a
ceda-dev-01 <l.lezzerini@coopceda.it>
2022-07-22 11:31:13 +0200

    Controller di prova

---
+++ server/src/main/java/it/iad/sigeba2/dto/DataNumeroDto.java
@@ -0,0 +1,32 @@
+package it.iad.sigeba2.dto;
+
+public class DataNumeroDto {
+
+    private String data;
+    private Integer numero;
+
+    public DataNumeroDto() {
+    }
+
+    public DataNumeroDto(String data, Integer numero) {
+        this.data = data;
+        this.numero = numero;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+}
