60a3e0aa942e475d2d8a639412228fce78ccb8c4
ceda-dev-01 <l.lezzerini@coopceda.it>
2022-07-22 10:39:36 +0200

    Prima versione server (vuoto)

---
+++ server/src/main/java/it/iad/sigeba2/Sigeba2Application.java
@@ -0,0 +1,13 @@
+package it.iad.sigeba2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sigeba2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sigeba2Application.class, args);
+	}
+
+}
---
+++ server/src/test/java/it/iad/sigeba2/Sigeba2ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.iad.sigeba2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sigeba2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
