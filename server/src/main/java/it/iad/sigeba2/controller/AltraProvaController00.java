449749e7e43d28ac9c982cb61d273af3badbb7f6
ceda-dev-01 <l.lezzerini@coopceda.it>
2022-07-29 09:28:51 +0200

    Primi casi d'uso

---
+++ server/src/main/java/it/iad/sigeba2/controller/AltraProvaController00.java
@@ -0,0 +1,19 @@
+package it.iad.sigeba2.controller;
+
+import it.iad.sigeba2.dto.SoloDataDto;
+import java.time.LocalDate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AltraProvaController00 {
+
+    @RequestMapping("/data00")
+    @ResponseBody
+    public SoloDataDto data00() {
+        String d = LocalDate.now().toString();
+        SoloDataDto sd = new SoloDataDto(d);
+        return sd;
+    }
+}
---
+++ server/src/main/java/it/iad/sigeba2/dto/SoloDataDto.java
@@ -0,0 +1,22 @@
+package it.iad.sigeba2.dto;
+
+public class SoloDataDto {
+
+    private String data;
+
+    public SoloDataDto() {
+    }
+
+    public SoloDataDto(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+}
