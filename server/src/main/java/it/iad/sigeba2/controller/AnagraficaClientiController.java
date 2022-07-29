6b105a99289fa9e5adb4d91b18456dac13116191
ceda-dev-01 <l.lezzerini@coopceda.it>
2022-07-29 11:03:02 +0200

    Update AnagraficaClientiController.java

--- server/src/main/java/it/iad/sigeba2/controller/AnagraficaClientiController.java
+++ server/src/main/java/it/iad/sigeba2/controller/AnagraficaClientiController.java
@@ -3,14 +3,17 @@ package it.iad.sigeba2.controller;
 import it.iad.sigeba2.dto.CriterioClienteDto;
 import it.iad.sigeba2.dto.ClienteDto;
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AnagraficaClientiController {
 
     @RequestMapping("/cercaCliente")
-    public List<ClienteDto> cercaCliente(CriterioClienteDto criterio) {
+    @ResponseBody
+    public List<ClienteDto> cercaCliente(@RequestBody CriterioClienteDto criterio) {
         throw new UnsupportedOperationException();
     }
 }
