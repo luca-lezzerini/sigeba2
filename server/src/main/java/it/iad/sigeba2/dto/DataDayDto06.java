7e5c45f565444221fc698428e04959aafa1cb660
FranCian1982 <96352321+FranCian1982@users.noreply.github.com>
2022-07-29 11:01:23 +0200

    esercizi casa

---
+++ server/src/main/java/it/iad/sigeba2/dto/DataDayDto06.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.iad.sigeba2.dto;
+
+/**
+ *
+ * @author ricca
+ */
+public class DataDayDto06 {
+    private String data;
+    
+
+    public DataDayDto06() {
+    }
+
+    public DataDayDto06(String data) {
+        this.data = data;
+       
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+   
+    
+}
+     
