416f392b5240b184519849be48e6a6fbaa4777cf
ceda-dev-01 <l.lezzerini@coopceda.it>
2022-08-01 17:46:21 +0200

    Creato controller mostraTuttiClienti

--- server/src/main/java/it/iad/sigeba2/controller/AnagraficaClientiController.java
+++ server/src/main/java/it/iad/sigeba2/controller/AnagraficaClientiController.java
@@ -6,6 +6,7 @@ import it.iad.sigeba2.dto.CriterioCancellazioneClienteDto;
 import it.iad.sigeba2.dto.CriterioModificaClienteDto;
 import it.iad.sigeba2.dto.SimpleIdDto;
 import it.iad.sigeba2.model.Cliente;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.extern.log4j.Log4j;
 import lombok.extern.log4j.Log4j2;
@@ -18,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 public class AnagraficaClientiController {
+
+    private final List<Cliente> clienti = new ArrayList<>();
 
     /* Il metodo prende in input un criterio di ricerca inserito dall'utente, tipicamente una stringa, 
     che potrebbe essere una parte del nome o del cognome o del codice fiscale del cliente da cercare. 
@@ -51,7 +54,12 @@ public class AnagraficaClientiController {
     @RequestMapping("/inserisciCliente")
 //creo il metodo inserisciCliente
     public List<ClienteDto> inserisciCliente(@RequestBody ClienteDto cliente) {
-        System.out.println("Entrato");
+        // riceve il DTO e lo trasforma in Cliente
+
+        // aggiunge il cliente alla lista dei clienti
+        
+        // chiama il metodo cercaCliente per ritornare la lista filtrata
+        // TODO: da rimuovere
         throw new UnsupportedOperationException();
     }
 
@@ -85,16 +93,39 @@ public class AnagraficaClientiController {
         throw new UnsupportedOperationException();
 
     }
-/**
- * Cancella il cliente dal DB.
- * Il cliente da cancellare viene identificato mediante 
- * @param criterio
- * @return 
- */
+
+    /**
+     * Cancella il cliente dal DB. Il cliente da cancellare viene identificato
+     * mediante
+     *
+     * @param criterio
+     * @return
+     */
     @RequestMapping("/cancellaCliente")
     @ResponseBody
     public List<ClienteDto> cancellaCliente(@RequestBody CriterioCancellazioneClienteDto criterio) {
         System.out.println("Entrato");
         throw new UnsupportedOperationException();
+    }
+
+    @RequestMapping("/mostraTuttiClienti")
+    @ResponseBody
+    public List<ClienteDto> mostraTuttiClienti() {
+        log.debug("Entrato in mostraTuttiClienti");
+        // crea la lista risultato (vuota)
+        List<ClienteDto> dtos = new ArrayList<>();
+        // copia tutti i clienti nei DTO
+        for (Cliente cliente : clienti) {
+            ClienteDto dto = new ClienteDto();
+            dto.setCodiceFiscale(cliente.getCodiceFiscale());
+            dto.setCognome(cliente.getCognome());
+            dto.setId(cliente.getId());
+            dto.setNome(cliente.getNome());
+            
+            dtos.add(dto);
+        }
+        log.debug("Uscito da mostraTuttiClienti");
+        // ritorna la lista dei DTO
+        return dtos;
     }
 }
--- server/src/main/java/it/iad/sigeba2/model/Cliente.java
+++ server/src/main/java/it/iad/sigeba2/model/Cliente.java
@@ -5,9 +5,9 @@ import lombok.Data;
 @Data
 public class Cliente {
 
+    private Long id;
     private String nome;
     private String cognome;
     private String codiceFiscale;
-    private String id;
 
 }
