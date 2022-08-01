23e15647371c366306605c66e4441f869d6e71a4
ceda-dev-01 <l.lezzerini@coopceda.it>
2022-08-01 17:05:14 +0200

    Update FerragostoController01.java

--- server/src/main/java/it/iad/sigeba2/controller/FerragostoController01.java
+++ server/src/main/java/it/iad/sigeba2/controller/FerragostoController01.java
@@ -10,21 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import it.iad.sigeba2.dto.GiornoFerragostoDto;
 
+@Deprecated
 @RestController
 public class FerragostoController01 {
-	
-	
-	@RequestMapping("giornoferragosto01")
-	@ResponseBody
-	public GiornoFerragostoDto giornoferragosto01() {
-		
-		LocalDate data = LocalDate.of(2022, 8, 15);
-		String giorno = data.getDayOfWeek().getDisplayName(TextStyle.FULL,Locale.ITALY).toString();
-		GiornoFerragostoDto dto = new GiornoFerragostoDto(data, giorno);
-		
-		return dto;
-		
-	}
-	
+
+    @RequestMapping("giornoferragosto01")
+    @ResponseBody
+    public GiornoFerragostoDto giornoferragosto01() {
+
+        LocalDate data = LocalDate.of(2022, 8, 15);
+        String giorno = data.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ITALY).toString();
+        GiornoFerragostoDto dto = new GiornoFerragostoDto(data, giorno);
+
+        return dto;
+
+    }
 
 }
