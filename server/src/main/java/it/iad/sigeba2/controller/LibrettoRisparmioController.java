2bfb91b4eea950587200213ff75e68f831eab06e
dsDario <102789951+dsDario@users.noreply.github.com>
2022-08-25 09:30:47 +0200

    Aggiunti commenti ai metodi LibrettoRisparmio #86
    Aggiunti commenti JavaDoc ai metodi in LibrettoRisparmioService e LibrettoRisparmioController

--- server/src/main/java/it/iad/sigeba2/controller/LibrettoRisparmioController.java
+++ server/src/main/java/it/iad/sigeba2/controller/LibrettoRisparmioController.java
@@ -27,6 +27,13 @@ public class LibrettoRisparmioController {
     @Autowired
     private LibrettoRisparmioService librettoRisparmioService;
     
+    /**
+     * Cerca i libretti che soddisfano il criterio di ricerca passato.
+     * @param criterio DTO con il criterio stringa, chiave parziale per il codice del libretto
+     * @return la lista dei libretti che soddisfano il criterio di ricerca passato,
+     * se non ne trova torna una lista vuota.
+     * @throws SigebaException 
+     */
     @RequestMapping("/cercaLibrettoRisparmio")
     @ResponseBody
     public RispostaConStato<List<LibrettoRisparmioDto>> cercaLibrettoRisparmio(@RequestBody CriterioLibrettoRisparmioDto criterio) {
@@ -44,6 +51,13 @@ public class LibrettoRisparmioController {
         return risp;
     }
 
+    /**
+     * Inserisce un nuovo libretto risparmio nel DB
+     * @param librettoRisparmioDaInserireDto il DTO con i dati del librettoRisparmio 
+     * da inserire (id deve essere null o zero) e il criterio di ricerca da applicare dopo l'inserimento
+     * @return la lista dei librettiRisparmio, dopo l'inserimento, che soddisfano il criterio passato
+     * @throws SigebaException 
+     */
     @RequestMapping("/inserisciLibrettoRisparmio")
     @ResponseBody
     public RispostaConStato<List<LibrettoRisparmioDto>> inserisciLibrettoRisparmio(@RequestBody CriterioInserimentoLibrettoRisparmioDto librettoRisparmioDaInserireDto) {
@@ -60,6 +74,12 @@ public class LibrettoRisparmioController {
         return risp;
     }
 
+    /**
+     * Cancella il librettoRisparmio dal DB. Il libretto da cancellare viene identificato mediante 
+     * @param dtoCancellazione che contiene l'id del libretto da cancellare
+     * @return la lista dei librettiRisparmio, dopo la rimozione.
+     * @throws SigebaException 
+     */
     @RequestMapping("/cancellaLibrettoRisparmio")
     @ResponseBody
     public RispostaConStato<List<LibrettoRisparmioDto>> cancellaLibrettoRisparmio(@RequestBody CriterioCancellazioneLibrettoRisparmioDto dtoCancellazione) {
@@ -76,6 +96,14 @@ public class LibrettoRisparmioController {
         return risp;
     }
 
+    /**
+     * Il metodo modifica il librettoRisparmio sostituendolo con quello passato 
+     * cercandolo tramite id
+     * @param modifica contiene il libretto da sostituire sul DB, e il criterio 
+     * per ritornare la lista dei librettiRisparmio aggiornata.
+     * @return la lista dei libretti aggiornata e filtrata con il criterio passato
+     * @throws SigebaException 
+     */
     @RequestMapping("/modificaLibrettoRisparmio")
     @ResponseBody
     public RispostaConStato<List<LibrettoRisparmioDto>> modificaLibrettoRisparmio(@RequestBody CriterioModificaLibrettoRisparmioDto modifica) {
@@ -92,6 +120,12 @@ public class LibrettoRisparmioController {
         return risp;
     }
 
+    /**
+     * Recupera i dati di un librettoRisparmio, fornito l'ID. Se non lo trova ritorna null.
+     * @param librettoRisparmioDto il DTO con l'ID del libretto da cercare (diverso da null e da 0)
+     * @return il libretto trovato o null se non lo trova
+     * @throws SigebaException 
+     */
     @RequestMapping("/leggiLibrettoRisparmio")
     @ResponseBody
     public LibrettoRisparmio leggiLibrettoRisparmio(@RequestBody SimpleIdDto librettoRisparmioDto) throws SigebaException {
--- server/src/main/java/it/iad/sigeba2/service/LibrettoRisparmioService.java
+++ server/src/main/java/it/iad/sigeba2/service/LibrettoRisparmioService.java
@@ -13,41 +13,45 @@ import java.util.List;
 public interface LibrettoRisparmioService {
  
     /**
-     * 
-     * @param criterio
-     * @return
+     * Cerca i libretti che soddisfano il criterio di ricerca passato.
+     * @param criterio DTO con il criterio stringa, chiave parziale per il codice del libretto
+     * @return la lista dei libretti che soddisfano il criterio di ricerca passato,
+     * se non ne trova torna una lista vuota.
      * @throws SigebaException 
      */
     List<LibrettoRisparmioDto> cercaLibrettoRisparmio(CriterioLibrettoRisparmioDto criterio) throws SigebaException;
     
     /**
-     * 
-     * @param librettoRisparmioDaInserireDto
-     * @return
+     * Inserisce un nuovo libretto risparmio nel DB
+     * @param librettoRisparmioDaInserireDto il DTO con i dati del librettoRisparmio 
+     * da inserire (id deve essere null o zero) e il criterio di ricerca da applicare dopo l'inserimento
+     * @return la lista dei librettiRisparmio, dopo l'inserimento, che soddisfano il criterio passato
      * @throws SigebaException 
      */
     List<LibrettoRisparmioDto> inserisciLibrettoRisparmio(CriterioInserimentoLibrettoRisparmioDto librettoRisparmioDaInserireDto) throws SigebaException;
     
     /**
-     * 
-     * @param dtoCancellazione
-     * @return
+     * Cancella il librettoRisparmio dal DB. Il libretto da cancellare viene identificato mediante 
+     * @param dtoCancellazione che contiene l'id del libretto da cancellare
+     * @return la lista dei librettiRisparmio, dopo la rimozione.
      * @throws SigebaException 
      */
     List<LibrettoRisparmioDto> cancellaLibrettoRisparmio(CriterioCancellazioneLibrettoRisparmioDto dtoCancellazione) throws SigebaException;
     
     /**
-     * 
-     * @param modifica
-     * @return
+     * Il metodo modifica il librettoRisparmio sostituendolo con quello passato cercandolo
+     * tramite id
+     * @param modifica contiene il libretto da sostituire sul DB, e il criterio 
+     * per ritornare la lista dei librettiRisparmio aggiornata.
+     * @return la lista dei libretti aggiornata e filtrata con il criterio passato
      * @throws SigebaException 
      */
     List<LibrettoRisparmioDto> modificaLibrettoRisparmio(CriterioModificaLibrettoRisparmioDto modifica) throws SigebaException;
     
     /**
-     * 
-     * @param librettoRisparmioDto
-     * @return
+     * Recupera i dati di un librettoRisparmio, fornito l'ID. Se non lo trova ritorna null.
+     * @param librettoRisparmioDto il DTO con l'ID del libretto da cercare (diverso da null e da 0)
+     * @return il libretto trovato o null se non lo trova
      * @throws SigebaException 
      */
     LibrettoRisparmio leggiLibrettoRisparmio(SimpleIdDto librettoRisparmioDto) throws SigebaException;
