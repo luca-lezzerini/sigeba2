c2752a8670954114d890f7fee33397c5cd06ba65
dsDario <102789951+dsDario@users.noreply.github.com>
2022-08-29 14:37:28 +0200

    StateCollector aggiornato

--- server/src/main/java/it/iad/sigeba2/helper/SigebaStateCollector.java
+++ server/src/main/java/it/iad/sigeba2/helper/SigebaStateCollector.java
@@ -8,8 +8,14 @@ import java.util.List;
 
 public class SigebaStateCollector {
 
-    public static final ThreadLocal<List<StatoRisposta>> contestoStati = new ThreadLocal<>();
+    private static final ThreadLocal<List<StatoRisposta>> contestoStati = new ThreadLocal<>();
 
+    /**
+     * Aggiunge un messaggio di stato in contestoStati con 
+     * @param messaggio Stringa che descrive la tipologia di segnalazione
+     * @param azione Stringa che descrive l'eventuale azione correttiva, se si tratta di un errore
+     * @param gravita Enum che ci descrive la gravità dell'eventuale errore
+     */
     public static void addStatusMessage(String messaggio, String azione, GravitaStatoEnum gravita) {
         StatoRisposta stato = new StatoRisposta(messaggio, azione, gravita);
         List<StatoRisposta> statiPrecedenti = contestoStati.get();
@@ -20,6 +26,10 @@ public class SigebaStateCollector {
         contestoStati.set(statiPrecedenti);
     }
 
+    /**
+     * 
+     * @return 
+     */
     public static List<StatoRisposta> getAndClean() {
         List<StatoRisposta> lista = contestoStati.get();
         if (lista == null) {
